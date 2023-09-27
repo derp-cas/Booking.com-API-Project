5de435d1d24fc1d110d8b153de53fffd770c13cf
derp-cas <kacperfps@googlemail.com>
2023-09-27 20:00:27 +0200

    bug on currencyPage fixed

--- src/app/currencyPage/components/ExchangeRates.tsx
+++ src/app/currencyPage/components/ExchangeRates.tsx
@@ -4,17 +4,23 @@ import { IExchangeRates } from "../types";
 const ExchangeRates: React.FC<IExchangeRates> = ({ exchange_rates }) => {
     return (
         <div className="currency-page__exchange-rates">
-            {exchange_rates.map((rate, index) => {
-                const { exchange_rate_buy, currency } = rate;
+            {exchange_rates
+                ? exchange_rates.map((rate, index) => {
+                      const { exchange_rate_buy, currency } = rate;
+                      const buyRate = parseFloat(exchange_rate_buy);
 
-                return (
-                    <div key={index} className="currency-page__exchange-rate">
-                        <p className="currency-page__rate">
-                            Buy for {exchange_rate_buy} {currency}
-                        </p>
-                    </div>
-                );
-            })}
+                      return (
+                          <div
+                              key={index}
+                              className="currency-page__exchange-rate"
+                          >
+                              <p className="currency-page__rate">
+                                  Buy for {buyRate.toFixed(2)} {currency}
+                              </p>
+                          </div>
+                      );
+                  })
+                : "Sorry, the currency is not available. Please try to reload the page"}
         </div>
     );
 };
--- src/app/currencyPage/types.ts
+++ src/app/currencyPage/types.ts
@@ -1,12 +1,14 @@
 export interface IExchangeRates {
-    exchange_rates: [
-        {
-            exchange_rate_buy: string;
-            currency: string;
-        },
-        {
-            currency: string;
-            exchange_rate_buy: string;
-        }
-    ];
+    exchange_rates:
+        | [
+              {
+                  exchange_rate_buy: string;
+                  currency: string;
+              },
+              {
+                  currency: string;
+                  exchange_rate_buy: string;
+              }
+          ]
+        | undefined;
 }
