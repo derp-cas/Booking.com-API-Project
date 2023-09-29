e308c8fbcdf281928ee9db03a0a471726fccbf2f
derp-cas <kacperfps@googlemail.com>
2023-09-29 15:31:32 +0200

    add fetch to onchange and refractor

--- src/app/currencyPage/components/CurrencyCalc.tsx
+++ src/app/currencyPage/components/CurrencyCalc.tsx
@@ -1,87 +1,94 @@
+import Loading from "@/app/assets/loading";
 import useFetch from "@/app/hooks/use-fetch";
-import React, { useState } from "react";
+import { log } from "console";
+import React, { useEffect, useState } from "react";
 
 const CurrencyCalc = () => {
-    const { data, isLoading } = useFetch("EUR");
+    const [firstCurrency, setFirstCurrency] = useState("USD");
+    const [secondCurrency, setSecondCurrency] = useState("EUR");
+    const { data, isLoading } = useFetch(firstCurrency);
     const { base_currency, exchange_rates } = data || {};
     const [amount, setAmount] = useState("");
-    const [firstCurrency, setFirstCurrency] = useState("");
-    const [secondCurrency, setSecondCurrency] = useState("");
+
+    useEffect(() => {
+        console.log(amount + " " + firstCurrency + " " + secondCurrency);
+        console.log(base_currency);
+    }, [firstCurrency]);
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log(amount + " " + firstCurrency + " " + secondCurrency);
-        const { data } = useFetch(firstCurrency);
     };
 
     return (
-        <form
-            className="currency-page__base-currency__form"
-            onSubmit={handleSubmit}
-        >
-            <label htmlFor="currency" className="form__label">
-                Amount:
-            </label>
-            <input
-                className="form__input"
-                name="amount"
-                type="number"
-                id="amount"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-            ></input>
-            <label htmlFor="currency" className="form__label">
-                From:
-            </label>
+        <>
             {exchange_rates ? (
-                <select
-                    name="currency"
-                    id="currency"
-                    className="form__select"
-                    value={firstCurrency}
-                    onChange={(e) => setFirstCurrency(e.target.value)}
+                <form
+                    className="currency-page__base-currency__form"
+                    onSubmit={handleSubmit}
                 >
-                    {exchange_rates.map((rate, index) => {
-                        const { currency } = rate;
+                    <label htmlFor="currency" className="form__label">
+                        Amount:
+                    </label>
+                    <input
+                        className="form__input"
+                        name="amount"
+                        type="number"
+                        id="amount"
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                    ></input>
+                    <label htmlFor="currency" className="form__label">
+                        From:
+                    </label>
 
-                        return (
-                            <option value={currency} key={index}>
-                                {currency}
-                            </option>
-                        );
-                    })}
-                </select>
-            ) : (
-                "Sorry, there was an error loading the exchange rates"
-            )}
-            <label htmlFor="currency" className="form__label">
-                To:
-            </label>
-            {exchange_rates ? (
-                <select
-                    name="currency"
-                    id="currency"
-                    className="form__select"
-                    value={secondCurrency}
-                    onChange={(e) => setSecondCurrency(e.target.value)}
-                >
-                    {exchange_rates.map((rate, index) => {
-                        const { currency } = rate;
+                    <select
+                        name="currency"
+                        id="currency"
+                        className="form__select"
+                        value={firstCurrency}
+                        onChange={(e) => setFirstCurrency(e.target.value)}
+                    >
+                        {exchange_rates.map((rate, index) => {
+                            const { currency } = rate;
 
-                        return (
-                            <option value={currency} key={index}>
-                                {currency}
-                            </option>
-                        );
-                    })}
-                </select>
+                            return (
+                                <option value={currency} key={index}>
+                                    {currency}
+                                </option>
+                            );
+                        })}
+                    </select>
+
+                    <label htmlFor="currency" className="form__label">
+                        To:
+                    </label>
+
+                    <select
+                        name="currency"
+                        id="currency"
+                        className="form__select"
+                        value={secondCurrency}
+                        onChange={(e) => setSecondCurrency(e.target.value)}
+                    >
+                        {exchange_rates.map((rate, index) => {
+                            const { currency } = rate;
+
+                            return (
+                                <option value={currency} key={index}>
+                                    {currency}
+                                </option>
+                            );
+                        })}
+                    </select>
+
+                    <button className="submit__button" type="submit">
+                        Calculate
+                    </button>
+                </form>
             ) : (
-                "Sorry, there was an error loading the exchange rates"
+                <Loading />
             )}
-            <button className="submit__button" type="submit">
-                Calculate
-            </button>
-        </form>
+        </>
     );
 };
 
